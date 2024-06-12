2d546691a5d32c0db1a4895d4ab424e8bb042c44
Cem Erdoğan <cem.cemerdogan97@gmail.com>
2024-06-12 21:56:49 +0300

    Card spawn
    1-) Card spawn based on world position

--- Assets/Scripts/Runtime/Managers/CardManager.cs
+++ Assets/Scripts/Runtime/Managers/CardManager.cs
@@ -3,10 +3,27 @@ using UnityEngine;
 public class CardManager : MonoBehaviour
 {
     [SerializeField] CardFactory cardFactory;
+    [SerializeField] float cellSize;
     Card[,] _cards;
+
+    private void Awake()
+    {
+        PrepareCards();
+    }
 
     void PrepareCards()
     {
+        for(int x = 0; x <= 5; x++)
+        {
+            for (int y = 0; y <= 5; y++)
+            {
+                cardFactory.CreateCard(GetWorldPosition(x, y, cellSize));
+            }
+        }
+    }
 
+    Vector3 GetWorldPosition(int x, int y, float cellSize)
+    {
+        return new Vector3 (x, y, 0) * cellSize;
     }
 }
