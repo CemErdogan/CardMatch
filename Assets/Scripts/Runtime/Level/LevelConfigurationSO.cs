688df307eed20f2d17eababd9165ddf93b72081c
Cem <cem.cemerdogan97@gmail.com>
2024-06-24 20:43:21 +0300

    Level configuratrion

---
+++ Assets/Scripts/Runtime/Level/LevelConfigurationSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewLevelConfiguration",
+    menuName = "ScriptableObjects/LevelConfiguration")]
+public class LevelConfigurationSO : ScriptableObject
+{
+    public LevelData[] levelData;
+}
