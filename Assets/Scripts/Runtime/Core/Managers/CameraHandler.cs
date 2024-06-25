6a16d37c44e6297cf7af2e7d4569cadcf79c3cf2
Cem <cem.cemerdogan97@gmail.com>
2024-06-25 22:02:18 +0300

    camera handler

--- Assets/Scripts/Runtime/Core/Managers/CameraHandler.cs
+++ Assets/Scripts/Runtime/Core/Managers/CameraHandler.cs
@@ -5,7 +5,7 @@ public class CameraHandler : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera vCam;
     [SerializeField] CinemachineTargetGroup targetGroup;
-    [SerializeField] float weight;
+    [SerializeField] float weight, offset;
 
     private void Awake()
     {
@@ -19,6 +19,39 @@ public class CameraHandler : MonoBehaviour
 
     void Prepare_Callback(Card[,] cards)
     {
+        var count = cards.GetLength(0) * cards.GetLength(1);
+        targetGroup.m_Targets = new CinemachineTargetGroup.Target[count];
 
+        var minX = float.MaxValue;
+        var maxX = float.MinValue;
+        var minY = float.MaxValue;
+        var maxY = float.MinValue;
+
+        foreach (var card in cards)
+        {
+            var cardPos = card.transform.position; 
+            if (cardPos.x < minX) minX = cardPos.x; 
+            if (cardPos.x > maxX) maxX = cardPos.x;
+            if (cardPos.y < minY) minY = cardPos.y;
+            if (cardPos.y > maxY) maxY = cardPos.y;
+        }
+
+        var distanceX = maxX - minX; 
+        var distanceY = maxY - minY;
+        var cameraSize = Mathf.Max(distanceX / 2, distanceY / 2); 
+
+        vCam.m_Lens.OrthographicSize = cameraSize + (weight * offset);
+
+        int counter = 0; 
+
+        for (int i = 0; i < cards.GetLength(0); i++)
+        {
+            for (int j = 0; j < cards.GetLength(1); j++)
+            {
+                targetGroup.m_Targets[counter].target = cards[i, j].transform;
+                targetGroup.m_Targets[counter].weight = weight;
+                counter++;
+            }
+        }
     }
 }
